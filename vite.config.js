330d019b9ddb18d5fc1e0725df0eb71b2593778f
vivinohradova2 <vynohradova.frontend@gmail.com>
2025-01-14 11:37:00 +0200

    fixed img

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://vivinohradova.github.io/game-Memory-flip",
+  base: "/game-Memory-flip/",
   plugins: [react()],
 });
